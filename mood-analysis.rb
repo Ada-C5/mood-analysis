732ea0de490586067aa93e55db1648b573cbc882
Laura Savino <lau.rasavino@gmail.com>
2016-04-01 10:11:45 -0700

    ok

--- mood-analysis.rb
+++ mood-analysis.rb
@@ -1,6 +1,6 @@
 FEELINGS = {
-  happy: %w(yay, good, great),
-  sad: %w(terrible, awful, horrible)
+  happy: %w(yay good great),
+  sad: %w(terrible awful horrible)
 }
 
 def analyze_mood(words)
@@ -19,6 +19,52 @@ def analyze_mood(words)
   return ":-|"
 end
 
+def strip_punctuation(words)
+  words.downcase.gsub(/[^0-9^a-z ]/, '')
+end
+
+def happy_days(day)
+  count = 0
+  day_record = 0
+  num_days_before_happy_three = 0
+  text.each do |aline|
+    count += 1
+    line = strip_punctuation(aline)
+    face = analyze_mood(line)
+    (day_record += 1) if face==":-)"
+    if day_record == 3 && num_days_before_happy_three == 0
+      num_days_before_happy_three = count
+    end
+  end
+  if num_days_before_happy_three == 0
+    puts "WHY ARE YOU SO SAD"
+  else
+    puts "It takes #{num_days_before_happy_three} entries for 3 happy days to occur"
+  end
+end
+
+def overall_mood
+  all = {
+    positive: 0,
+    negative: 0
+    meh: 0
+  }
+  text.each do |aline|
+    line = strip_punctuation(aline)
+    face = analyze_mood(line)
+    if face == ":-)"
+      all[:positive] +=1
+    if face == ":-("
+      all[:negative] +=1
+    else
+      all[:meh] +=1
+    end
+  end
+  largest = all.max_by{|key, value| value}
+  puts "#{largest.keys} is most common mood" 
+end
+
+
 text = [
   "03/01 I'm having a terrible horrible no good day.",
   "03/13 Yesterday was horrible, but today is great! Yay!",
@@ -28,5 +74,8 @@ text = [
   "05/11 Yay, yay, yay! I'm having a awfuly great day."
 ]
 
-puts analyze_mood(text[0])
-puts analyze_mood(text[1])
+text.each do |aline|
+  line = strip_punctuation(aline)
+  face = analyze_mood(line)
+  puts "#{line[0,2]}/#{line[2,3]} #{face}"
+end
